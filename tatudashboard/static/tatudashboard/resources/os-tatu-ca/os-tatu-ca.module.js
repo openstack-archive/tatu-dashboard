5c1cba79d8f54e0e7ca3f02841e67c5dc257ea9d
Pino de Candia <giuseppe.decandia@gmail.com>
2018-01-12 23:20:30 +0000

    More attempts.

---
+++ tatudashboard/static/tatudashboard/resources/os-tatu-ca/os-tatu-ca.module.js
@@ -0,0 +1,78 @@
+/**
+ * (c) Copyright 2016 Hewlett Packard Enterprise Development LP
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+(function () {
+  'use strict';
+
+  /**
+   * @ngdoc overview
+   * @ngname tatudashboard.resources.os-tatu-host
+   *
+   * @description
+   * Provides all of the services and widgets required
+   * to support and display SSH (Tatu) host related content.
+   */
+  angular
+    .module('tatudashboard.resources.os-tatu-ca', [
+      'ngRoute',
+    ])
+    .constant(
+      'tatudashboard.resources.os-tatu-host.resourceType', 'OS::Tatu::CA')
+    .run(run);
+
+  run.$inject = [
+    'horizon.framework.conf.resource-type-registry.service',
+    'tatudashboard.resources.os-tatu-host.resourceType'
+  ];
+
+  function run(registry,
+               resourceTypeString) {
+    var resourceType = registry.getResourceType(resourceTypeString);
+    resourceType
+      .setNames(gettext('SSH CA'), gettext('SSH CAs'))
+      .setListFunction(listCAs)
+      .setProperty('id', {
+        label: gettext('CA ID')
+      })
+      .setProperty('host_pub_key', {
+        label: gettext('Host Public Key'),
+      })
+      .setProperty('user_pub_key', {
+        label: gettext('User Public Key'),
+      });
+
+    resourceType
+      .tableColumns
+      .append({
+        id: 'id',
+        priority: 1
+      })
+      .append({
+        id: 'host_pub_key',
+        priority: 2,
+      })
+      .append({
+        id: 'user_pub_key',
+        priority: 2,
+      });
+
+
+    function listHosts() {
+      return [];
+    }
+  }
+
+})();
